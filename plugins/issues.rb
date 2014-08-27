83cff012ab264bc8359bc449d5d791ec3b5bb725
Jonne Haß <me@mrzyx.de>
2014-08-27 23:27:35 +0200

    match issues on word bondary

--- plugins/issues.rb
+++ plugins/issues.rb
@@ -6,7 +6,7 @@ class Issues
   listen_to :channel
   def listen(m)
     return unless PROJECTS.has_key? m.channel.name
-    issues = m.message.scan(/(?:^|\s+)#(\d{4,5})(?:\s+|$)/).map(&:first)
+    issues = m.message.scan(/(?:^|\s+|\()#(\d{4,5})\b/).map(&:first)
     return if issues.empty?
     m.reply issues.map {|issue| "#{PROJECTS[m.channel.name]}#{issue}" }.join(' | ')
   end

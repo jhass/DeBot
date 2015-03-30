e13ad70c5658c7b4ac5f63f70a9d07b0f31cc43b
Jonne Haß <me@jhass.eu>
2015-03-30 22:11:15 +0200

    added simple bot

---
+++ bot/src/simple.cr
@@ -0,0 +1,21 @@
+require "framework/bot"
+require "framework/plugin"
+
+class HelloWorld
+  include Framework::Plugin
+
+  match /!hello_world/
+  def execute msg, match
+    msg.reply "Hi #{msg.sender.nick}!"
+  end
+end
+
+bot = Framework::Bot.create do
+  config.server = "chat.freenode.net"
+  config.nick = "CrystalBot"
+  config.channels = ["##cebot"]
+
+  add_plugin HelloWorld
+end
+
+bot.start

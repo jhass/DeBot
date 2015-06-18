94ed6dacc78581a3f90d7e5abad43b3fae92edd7
Thor77 <xXThor77Xx@gmail.com>
2015-06-18 15:11:40 +0200

    Improve github_issue-plugin check if issue exists and show title

--- bot/src/plugins/github_issues.cr
+++ bot/src/plugins/github_issues.cr
@@ -1,7 +1,21 @@
 require "framework/plugin"
+require "http/client"
+require "json"
 
 class GithubIssues
   include Framework::Plugin
+
+  class Issue
+    json_mapping({
+        html_url: String,
+        title: String
+    })
+
+    def to_s(io)
+      io << html_url << " (#{title})"
+    end
+  end
+
 
   PATTERN = /(?:^|\s+|\()#(\d{3,5})\b/
 
@@ -14,6 +28,14 @@ class GithubIssues
     return unless msg.channel?
     return unless config.repositories.has_key? msg.channel.name
     issues = msg.message.scan(PATTERN)
-    msg.reply issues.map {|issue| "https://github.com/#{config.repositories[msg.channel.name]}/issues/#{issue[1]}" }.join(" | ")
+    msg.reply issues.map {|issue| fetch_issue config.repositories[msg.channel.name], issue[1]}.compact.join(" | ")
+  end
+
+  private def fetch_issue repo, issue_id
+    api_response = HTTP::Client.get("https://api.github.com/repos/#{repo}/issues/#{issue_id}",
+      headers: HTTP::Headers{"Accept" => ["application/vnd.github.v3+json"], "User-Agent" => ["CeBot"]})
+    if api_response.status_code == 200
+      Issue.from_json api_response.body
+    end
   end
 end
